ff9f3cda22be58323643b889ea488f3e3b682505
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-17 22:29:56 -0400

    Creando UsuariosController

---
+++ VehiculosAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vehiculos.Abstractions;
+using Vehiculos.Data.Models;
+using Vehiculos.Domain.DTO;
+
+namespace VehiculosAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsuariosController(IUsuariosService usuariosService) : ControllerBase
+{
+    // GET: api/Usuarios
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UsuariosDto>>> GetUsuarios()
+    {
+        return await usuariosService.Listar(p => true);
+    }
+
+    // GET: api/Usuarios/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UsuariosDto>> GetUsuarios(int id)
+    {
+        return await usuariosService.Buscar(id);
+    }
+
+    // PUT: api/Usuarios/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutUsuarios(int id, UsuariosDto usuariosDto)
+    {
+        if (id != usuariosDto.UsuarioId)
+        {
+            return BadRequest();
+        }
+
+        await usuariosService.Guardar(usuariosDto);
+
+        return NoContent();
+    }
+
+    // POST: api/Usuarios
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Usuario>> PostUsuarios(UsuariosDto usuariosDto)
+    {
+        await usuariosService.Guardar(usuariosDto);
+
+        return CreatedAtAction("GetUsuarios", new { id = usuariosDto.UsuarioId }, usuariosDto);
+    }
+
+    // DELETE: api/Usuarios/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUsuarios(int id)
+    {
+        await usuariosService.Eliminar(id);
+
+        return NoContent();
+    }
+}
