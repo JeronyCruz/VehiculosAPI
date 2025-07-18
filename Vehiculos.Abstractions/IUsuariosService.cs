5b6a8c55b5db0de7b45c3b5f4b26b8e0c53ffbc8
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-17 22:28:40 -0400

    Creando IUsuariosService

---
+++ Vehiculos.Abstractions/IUsuariosService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Vehiculos.Domain.DTO;
+
+namespace Vehiculos.Abstractions;
+
+public interface IUsuariosService
+{
+    Task<bool> Guardar(UsuariosDto usuario);
+    Task<bool> Eliminar(int usuarioId);
+    Task<UsuariosDto> Buscar(int id);
+    Task<List<UsuariosDto>> Listar(Expression<Func<UsuariosDto, bool>> criterio);
+    Task<bool> ExisteUsusario(int id, string nombre);
+}
