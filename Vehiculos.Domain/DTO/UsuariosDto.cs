ccb1059681c9404d0bdefbbdcb2c7890660c8a2b
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-17 22:27:28 -0400

    Creando Dto de Usuarios

---
+++ Vehiculos.Domain/DTO/UsuariosDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculos.Domain.DTO;
+
+public class UsuariosDto
+{
+    public int UsuarioId { get; set; }
+    public string? Nombre { get; set; }
+    public double Balance { get; set; }
+}
