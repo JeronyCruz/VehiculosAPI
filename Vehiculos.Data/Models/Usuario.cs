f74db8548f62ec92cc3325464b46cb8d58e2d251
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-17 22:26:36 -0400

    Creando Modelo Usuario

---
+++ Vehiculos.Data/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculos.Data.Models;
+
+public class Usuario
+{
+    [Key]
+    public int UsuarioId { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras")]
+    public string? Nombre { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten numeros enteros o decimales")]
+    public double Balance { get; set; }
+}
